835f9a7aef7ff3390212a664aca1c575b585681a
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-09-03 18:21:30 +0530

    ORM hibernate

---
+++ Relationships/src/main/java/com/JPA/Relationships/Entity/HibernateRelationalMapping.java
@@ -0,0 +1,30 @@
+package com.JPA.Relationships.Entity;
+
+public class HibernateRelationalMapping {
+
+/**
+ * Relational Mapping (ORM) means:	👉 Converting Java Objects ↔ Database Tables
+									Java class → Table
+									Object → Row
+									Fields → Columns
+   This is handled by:
+					JPA (Specification)
+					Hibernate (Implementation of JPA)
+ * 👉 ORM does: SQL generation, Mapping, Data conversion
+ * 
+ * 🔷 Types of Relationships: 1️ One-to-One Mapping, 2️ One-to-Many Mapping, 3️ Many-to-One Mapping
+ * 													4️ Many-to-Many Mapping
+ * 
+ * 	🔷 How It Works Internally :
+ *			You create entity classes -> Hibernate reads annotations -> Generates tables
+ * 			-> Manages relationships using:  Foreign keys, Join tables
+ * 
+ * 
+ * 
+ * 
+ * 
+
+ */
+	
+	
+}
